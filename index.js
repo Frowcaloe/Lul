e77654458427e10102a9e9dc41f23b39c6f34a7b
Frowcaloe <forgame061601@gmail.com>
2024-09-06 11:52:27 +0800

    index.js

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const genText = document.getElementById("gen-text")
 
 genText.addEventListener("click", function() {
   let messages = [
-    "Kainin!",
-    "Mo",
-    "Titi",
-    "Ko",
-    "I love you",
+    "Kainin mo titi ko!",
   ]
   const textEl = document.querySelector(".textMessage")
   textEl.textContent = messages[Math.floor( Math.random()*messages.length )]

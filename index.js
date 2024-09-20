9ad7f5eae22c8ddf40b7697acab4c00fca4380b6
Frowcaloe <forgame061601@gmail.com>
2024-09-20 17:57:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ genText.addEventListener("click", function() {
   let messages = [
     "Ceejay",
     "Hendrix",
-    "Fil major",
+    "Joey",
   ]
   const textEl = document.querySelector(".textMessage")
   textEl.textContent = messages[Math.floor( Math.random()*messages.length )]
